9ebd54af607c7cf2067ab364a3835c766a51bc67
buibuibui9527 <buibuibui95@gmail.com>
2026-02-14 01:44:41 +0800

    fix: implement next-intl middleware and nested routing

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import createMiddleware from 'next-intl/middleware';
 
-export function middleware(request: NextRequest) {
-  return NextResponse.next()
-}
+export default createMiddleware({
+  // 設定支援的語系，必須與你 messages 資料夾內的檔案對應
+  locales: ['en', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pt', 'zh-CN', 'zh-TW'],
+  
+  // 如果使用者沒指定語系，預設使用的語系
+  defaultLocale: 'zh-TW'
+});
 
 export const config = {
+  // 匹配所有路徑，除了 api、_next、以及含有句點的靜態檔案（如 .png）
   matcher: ['/((?!api|_next|.*\\..*).*)']
-}
+};

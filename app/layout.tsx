36de9ff268bd4a4c83b87405a45da8e63c1412be
buibuibui9527 <buibuibui95@gmail.com>
2026-02-14 01:14:12 +0800

    fix: correct css import path and move i18n messages to folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 import { ReactNode } from 'react'
-import '../globals.css'
+import './globals.css'
 
 export default async function LocaleLayout({
   children,
